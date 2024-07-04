b8bc316bd6464b0a62ab6c5643c353488adbc334
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-04 16:04:59 -0600

    chore: Update SQL queries for string manipulation and documentation

---
+++ 08CASE.sql
@@ -0,0 +1,27 @@
+-- CASE 
+-- Along with the fullname, salary of the employee, print whether the employee belongs to high salary category or low
+-- Salary > 48000 is consider High
+
+-- Remember If you start a case, you have to end it as well; WHEN-THEN-ELSE
+
+-- SELECT CONCAT_WS(' ', fname, lname) AS fullname, salary,
+-- CASE 
+-- 	WHEN salary >= 48000 THEN 'HIGH'
+-- 	ELSE 'LOW'
+-- END AS salary_category	
+-- FROM employees;
+
+
+
+-- If the employee falls in the low salary category give him 10% bonus;
+-- SELECT CONCAT_WS(' ', fname, lname), salary,
+-- CASE
+--   WHEN salary>=48000 THEN 'HIGH'
+--   ELSE 'LOW'
+-- END AS salary_category,
+-- CASE 
+--   WHEN salary < 48000 THEN ROUND(salary*0.10)
+--   ELSE 0
+-- END AS bonus
+-- FROM employees;
+
